4d60410a06c73b9d66a0a15e2f4f19447265f2ef
Lewis Watson <lewis.watson@comcarde.com>
2019-09-25 06:46:57 +0100

    Tweak comments and make some variables final

--- src/test/java/com/github/lewiswatson/camelcatbroker/route/CatBrokerRouteSpringBootTest.java
+++ src/test/java/com/github/lewiswatson/camelcatbroker/route/CatBrokerRouteSpringBootTest.java
@@ -54,7 +54,7 @@ public class CatBrokerRouteSpringBootTest {
     dlqEndpoint.expectedBodiesReceived(scratchyBritishBlueJson);
 
     /*
-     * When
+     * when
      */
 
     testProducer.sendBody(biteyTabbyCatJson);
